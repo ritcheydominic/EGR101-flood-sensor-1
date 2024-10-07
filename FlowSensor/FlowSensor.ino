1b3279dfcb93f06f6eb3c5a7f13698f3070dfcf2
Dominic Ritchey <dominic@ritchey.family>
2024-10-06 20:49:42 -0500

    Move Arduino code to own folder
    Make Arduino IDE happy

---
+++ FlowSensor/FlowSensor.ino
@@ -0,0 +1,86 @@
+/*
+   YF-S201 Hall Effect Water Flow Meter / Sensor
+   http://www.hobbytronics.co.uk/yf-s201-water-flow-meter
+   
+   Read Water Flow Meter and output reading in litres/hour
+
+*/
+
+volatile int  flow_frequency;  // Measures flow meter pulses
+unsigned int  l_hour;          // Calculated litres/hour                      
+unsigned char flowmeter = 2;  // Flow Meter Pin number
+unsigned long currentTime;
+unsigned long cloopTime;
+
+#include <SPI.h>
+#include <WiFiNINA.h>
+#include <BlynkSimpleWifi.h>
+
+#define echoPin 2 // attach pin D2 Arduino to pin Echo of HC-SR04
+#define trigPin 3 //attach pin D3 Arduino to pin Trig of HC-SR04
+
+/* Comment this out to disable prints and save space */
+#define BLYNK_PRINT Serial
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "EuT94b8qqQeyB9ZG8lzkY0pSHwh8J-bZ";
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "G8P3X128";
+char pass[] = "yeeyeeland";
+
+// defines variables
+long duration; // variable for the duration of sound wave travel
+int distance; // variable for the distance measurement
+
+BlynkTimer timer;
+
+// This function sends Arduino's up time every second to Virtual Pin (5).
+// In the app, Widget's reading frequency should be set to PUSH. This means
+// that you define how often to send data to Blynk App.
+void myTimerEvent()
+{
+  // You can send any value at any time.
+  // Please don't send more that 10 values per second.
+  Blynk.virtualWrite(V2, distance);
+}
+
+void flow ()                  // Interruot function
+{ 
+   flow_frequency++;
+} 
+
+void setup()
+{ 
+   pinMode(flowmeter, INPUT);
+   Serial.begin(9600); 
+   attachInterrupt(0, flow, RISING); // Setup Interrupt 
+                                     // see http://arduino.cc/en/Reference/attachInterrupt
+   sei();                            // Enable interrupts  
+   currentTime = millis();
+   cloopTime = currentTime;
+
+   Blynk.begin(auth, ssid, pass);
+   timer.setInterval(1000L, myTimerEvent);
+} 
+
+void loop ()    
+{
+   currentTime = millis();
+   // Every second, calculate and print litres/hour
+   if(currentTime >= (cloopTime + 1000))
+   {     
+      cloopTime = currentTime;              // Updates cloopTime
+      // Pulse frequency (Hz) = 7.5Q, Q is flow rate in L/min. (Results in +/- 3% range)
+      l_hour = (flow_frequency * 60 / 7.5); // (Pulse frequency x 60 min) / 7.5Q = flow rate in L/hour 
+      flow_frequency = 0;                   // Reset Counter
+//      Serial.print("0 L/hour");
+//      Serial.print(l_hour, DEC);            // Print litres/hour
+//      Serial.println(" L/hour");
+   }
+
+   Blynk.run();
+   timer.run();
+}
